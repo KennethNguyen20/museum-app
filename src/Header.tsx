6819848e7b81540085e624ada0a16fe2668a6609
Kenneth Nguyen <kennethnguyen007@gmail.com>
2025-05-13 17:02:25 -0400

    Initial commit

---
+++ src/Header.tsx
@@ -0,0 +1,7 @@
+// src/Header.tsx
+import React from 'react';
+import styles from './Header.module.css';
+
+export default function Header() {
+  return <h1 className={styles.title}>Welcome to Museum App</h1>;
+}
